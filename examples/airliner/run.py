59f90f401f9908bf75c920f833e50415354c9cf1
nichco <norndorff@live.com>
2026-02-19 22:16:32 -0800

    some progress

--- examples/airliner/run.py
+++ examples/airliner/run.py
@@ -1,59 +1,74 @@
 from optiplex import Plex
 import numpy as np
-from sub_problem import make_sub_problem
-from constraint import constraint
-import matplotlib.pyplot as plt
+from sub_p_funs import make_sub_problem
+from typing import List
+from optiplex import combo
+import jax.numpy as jnp
 
-N = 2
-mission_ranges = np.linspace(1.5e6, 3e6, N)
+# N = 2
+# mission_ranges = np.linspace(1.5e6, 3e6, N)
 
-subP_functions = []
-for i in range(N):
-    subPfunc = make_sub_problem(mission_ranges[i], i, N)
-    subP_functions.append(subPfunc)
+# subP_functions = []
+# for i in range(N):
+#     subPfunc = make_sub_problem(mission_ranges[i], i, N)
+#     subP_functions.append(subPfunc)
 
 
-nu = 60
-eta0 = np.linspace(0.6, 0.2, nu)
-theta0 = np.linspace(np.deg2rad(3), np.deg2rad(1), nu)
-tf0 = np.array([7000.0])
-b0 = np.array([32.0])
+# nu = 60
+# eta0 = np.linspace(0.6, 0.2, nu)
+# theta0 = np.linspace(np.deg2rad(3), np.deg2rad(1), nu)
+# tf0 = np.array([7000.0])
+# b0 = np.array([32.0])
 
-v_init = []
-# automate the construction of v_init for changing N
-for i in range(N): 
-    v_init.append(eta0)
-    v_init.append(theta0)
-    v_init.append(tf0)
-    v_init.append(b0)
+# v_init = []
+# # automate the construction of v_init for changing N
+# for i in range(N): 
+#     v_init.append(eta0)
+#     v_init.append(theta0)
+#     v_init.append(tf0)
+#     v_init.append(b0)
 
-opt = Plex(blocks=subP_functions,
+N = 4
+rvals = np.linspace(1e6, 6e6, N) # mission range values
+
+subPfuns = []
+for i, r in enumerate(rvals):
+    subPfuns.append(make_sub_problem(r))
+
+
+
+
+# pair-wise differences formulation
+def constraint(x_init: List[np.ndarray]) -> jnp.ndarray:
+    
+    # l1 = x_init[0] # need to expand for changing N
+    # l2 = x_init[1] 
+    # mp1 = x_init[2]
+    # mp2 = x_init[3]
+
+    AR_list = [x_init[index] for ...]
+    S_list = [x_init[index] for ...]
+
+    AR_constraint = combo(AR_list)
+    S_constraint = combo(S_list)
+    return jnp.concatenate((AR_constraint, S_constraint))
+
+
+
+opt = Plex(subproblems=subPfuns,
            x_init=v_init,
-           constraint=constraint)
+           con=constraint,
+           )
 
-opt.mu = 1e-2
+opt.solve(max_outer_iter=10,
+          max_inner_iter=2,
+          ATOL_out=1e-5, 
+          RTOL_out=1e-5,
+          ATOL_in=1e-2, 
+          RTOL_in=1e-2,
+          ATOL_feas=1e-3,
+          rho=1.05
+          )
 
-opt.solve(max_iter=30, 
-          rho=1.5,
-          tol=1e-2,
-          ctol=1e-3)
-
-print("Success:", opt.success)
-print('Iterations: ', opt.num_iter)
+# print('Solution: ', opt.x)
 print('Time (s): ', opt.time)
-
-
-x_init = opt.x_init
-b1 = x_init[3]
-b2 = x_init[7]
-print('b1: ', b1, 'b2: ', b2)
-
-diffs = opt.diffs
-constraint_violations = opt.constraint_violations
-
-
-plt.semilogy(diffs, label='Max Relative Diff')
-plt.semilogy(constraint_violations, label='Max Constraint Violation')
-plt.xlabel('Iteration', fontsize=14)
-plt.grid(color='lavender', alpha=0.5)
-plt.show()
