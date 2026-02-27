31fe013bf2ef4cf7a4848b35dd4ce8190622bc59
nichco <norndorff@live.com>
2026-02-27 10:58:18 -0800

    pdify

---
+++ warm_start/pdify.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+
+
+def eig_clip():
+    pass
+
+
+def eig_flip():
+    pass
+
+
+def add_diag_a():
+    # https://math.stackexchange.com/questions/332456/how-to-make-a-matrix-positive-semidefinite
+    pass
+
+
+def add_diag_b():
+    # https://nhigham.com/2021/02/16/diagonally-perturbing-a-symmetric-matrix-to-make-it-positive-definite/
+    pass
+
+
+def nearest():
+    # https://nhigham.com/2021/01/26/what-is-the-nearest-positive-semidefinite-matrix/
+    pass
