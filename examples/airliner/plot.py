72e052c042ea983d66b1e68d694bb453564ed05f
nichco <norndorff@live.com>
2026-03-01 20:53:10 -0800

    data

--- examples/airliner/plot.py
+++ examples/airliner/plot.py
@@ -2,18 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-monolithic_N = np.array([2, 4, 6, 8, 20])
-monolithic_times = np.array([94.2, 263.8, 580.6, 762.9, 5162.2])
+monolithic_N = np.array([2, 4, 6, 8])
+monolithic_times = np.array([189.4, 1084.4, 3755.1, 8765.8])
 
 distributed_N = np.array([2, 4, 6, 8, 10])
-distributed_times = np.array([298.4, 534.8, 937.5, 1551.2, 2227.4])
+distributed_times = np.array([99.3, 145.9, 194.8, 284.0, 343.8])
 
 
 
-plt.semilogy(monolithic_N, monolithic_times, marker='o', label='Monolithic')
-plt.semilogy(distributed_N, distributed_times, marker='o', label='Distributed')
+plt.figure(figsize=(4,3))
+
+plt.semilogy(monolithic_N, monolithic_times, marker='o', linewidth=1, label='Monolithic')
+plt.semilogy(distributed_N, distributed_times, marker='s', linewidth=1, label='Distributed')
 
 plt.xlabel('Number of Subproblems')
 plt.ylabel('Time (s)')
 
+plt.legend(loc='upper left', fontsize=10)
+
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_color('gray')
+ax.spines['left'].set_color('gray')
+ax.spines['bottom'].set_position(('outward', 6))
+ax.spines['left'].set_position(('outward', 6))
+
+plt.savefig('bwb_time_scaling.pdf', bbox_inches='tight')
 plt.show()
