af61bb3137c0925b82187227f77fddb93b3138b4
nichco <norndorff@live.com>
2026-03-08 15:10:23 -0700

    plotting

---
+++ examples/cart_pole/memory_plot.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+copies = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# monolithic_memory_csdl = np.array([53.302, 73.99, 100.646, 132.946, 170.566, 213.768, 262.973, 318.20, 377.307])
+monolithic_memory_jax = np.array([11.063, 21.581, 35.208, 52.54, 73.571, 98.296, 126.656, 158.705, 193.573])
+
+distributed_memory = np.array([5.006, 5.116, 5.33, 5.497, 5.641, 5.864, 6.134, 6.368, 6.657])
+
+
+
+
+plt.figure(figsize=(4,3))
+
+plt.semilogy(copies, monolithic_memory_jax, label='Monolithic', marker='o', markersize=7, linewidth=1, color='tab:gray')
+plt.semilogy(copies, distributed_memory, label='Distributed', marker='s', markersize=7, linewidth=1, color='tab:blue')
+plt.xlabel('Number of sub-problems')
+plt.ylabel('Memory (MB)')
+plt.legend()
+
+plt.grid(True, linewidth=0.5, alpha=0.3)
+
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_color('gray')
+ax.spines['left'].set_color('gray')
+ax.spines['bottom'].set_position(('outward', 6))
+ax.spines['left'].set_position(('outward', 6))
+
+plt.savefig('cart_pole_memory.pdf', transparent=True, bbox_inches='tight')
+plt.show()
---
+++ examples/cart_pole/timing_plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+subproblems = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# monolithic_time = np.array([11.626, 42.037, 133.012, 621.379, 1467.451, 2944.026, 6370])
+monolithic_time = np.array([9.86, 24.22, 112.345, 386.23, 755.009, 1504.773, 2785.930, 3508.666, 15755.439])
+
+# distributed_time = np.array([52.335, 88.211, 205.231, 161.269, 395.084, 429.445, 578.767])
+distributed_time = np.array([40.675, 58.815, 176.175, 265.7, 320.679, 399.415, 565.788, 588.668, 817.812])
+
+plt.figure(figsize=(4,3))
+
+plt.semilogy(subproblems, monolithic_time, label='Monolithic', marker='o', markersize=7, linewidth=1, color='tab:gray')
+plt.semilogy(subproblems, distributed_time, label='Distributed', marker='s', markersize=7, linewidth=1, color='tab:blue')
+plt.xlabel('Number of subproblems')
+plt.ylabel('Time (s)')
+plt.legend()
+plt.grid(True, linewidth=0.5, alpha=0.3)
+
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_color('gray')
+ax.spines['left'].set_color('gray')
+ax.spines['bottom'].set_position(('outward', 6))
+ax.spines['left'].set_position(('outward', 6))
+
+plt.savefig('cart_pole_timing.pdf', transparent=True, bbox_inches='tight')
+plt.show()
