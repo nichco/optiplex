54f0056030a81ce76582399abad283859ca08f44
nichco <norndorff@live.com>
2026-04-23 13:11:25 -0700

    init update

--- optiplex/__init__.py
+++ optiplex/__init__.py
@@ -2,5 +2,6 @@ from .core.plex import Plex
 from .core.warm import Plex as WarmPlex
 from .core.h5plex import Plex as H5Plex
 from .core.plexT import Plex as PlexT
+from .core.plexC import Plex as PlexC
 # from .core.decomp import DuPlex
 from .utils.combo import combo
