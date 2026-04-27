0126fd5eef71d7ac6dd44d811673f25c8769fe13
nichco <norndorff@live.com>
2026-04-27 16:10:42 -0700

    works

--- examples/aero_structural/run_idf_dev_4.py
+++ examples/aero_structural/run_idf_dev_4.py
@@ -67,7 +67,7 @@ f_scale = 1e-2
 disp_scale = 1e1
 """
 lw_scale = 5e-5
-f_scale = 2e-4#1e-4 (term)
+f_scale = 2e-4
 disp_scale = 1e-1#9e-2 ( this and new f_scale in vscode)
 
 # initial design variable values
