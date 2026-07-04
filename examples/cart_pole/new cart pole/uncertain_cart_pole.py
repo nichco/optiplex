ecd7dc59d4b733b70d3953eee67f645de6900084
nichco <norndorff@live.com>
2026-07-03 23:09:23 -0700

    oh

--- examples/cart_pole/new cart pole/uncertain_cart_pole.py
+++ examples/cart_pole/new cart pole/uncertain_cart_pole.py
@@ -9,18 +9,18 @@ from scipy.stats.qmc import LatinHypercube, scale
 
 # sample the space of uncertain parameters using Latin hypercube sampling
 # uncertain parameters: g, mu_cart, mu_pole
-g_u = 9.81 + 0.001
-g_l = 9.81 - 0.001
-mu_cart_u = 0.03 + 0.0001
-mu_cart_l = 0.03 - 0.0001
-mu_pole_u = 0.03 + 0.0001
-mu_pole_l = 0.03 - 0.0001
-N = 2
-# sampler = LatinHypercube(d=3, seed=0)
-# samples = scale(sampler.random(N), 
-#                 l_bounds=[g_l, mu_cart_l, mu_pole_l], 
-#                 u_bounds=[g_u, mu_cart_u, mu_pole_u])
-samples = np.array([[9.81, 0.03, 0.03], [9.81, 0.03, 0.03]])
+g_u = 9.81 + 0.1
+g_l = 9.81 - 0.1
+mu_cart_u = 0.03 + 0.01
+mu_cart_l = 0.03 - 0.01
+mu_pole_u = 0.03 + 0.01
+mu_pole_l = 0.03 - 0.01
+N = 5
+sampler = LatinHypercube(d=3, seed=0)
+samples = scale(sampler.random(N), 
+                l_bounds=[g_l, mu_cart_l, mu_pole_l], 
+                u_bounds=[g_u, mu_cart_u, mu_pole_u])
+# samples = np.array([[9.81, 0.03, 0.03], [9.81, 0.03, 0.03]])
 print(samples)
 
 # parameters
@@ -160,7 +160,7 @@ o_scaler = 1e-2
 
 jaxprob = JaxProblem(x0=v0, jax_obj=jax_obj, jax_con=jax_con, cl=cl, cu=cu, 
                      xl=xl, xu=xu, o_scaler=o_scaler, c_scaler=c_scaler, x_scaler=x_scaler)
-optimizer = SLSQP(jaxprob, solver_options={'maxiter': 1000, 'ftol': 1e-8}, turn_off_outputs=True)
+optimizer = SLSQP(jaxprob, solver_options={'maxiter': 3000, 'ftol': 1e-8}, turn_off_outputs=True)
 optimizer.solve()
 optimizer.print_results()
 ans = optimizer.results['x'] / x_scaler
--- examples/cart_pole/new cart pole/uncertain_cart_pole_bsplines.py
+++ examples/cart_pole/new cart pole/uncertain_cart_pole_bsplines.py
@@ -10,18 +10,18 @@ from jax_b_splines import get_bspline_mtx, bspline_comp
 
 # sample the space of uncertain parameters using Latin hypercube sampling
 # uncertain parameters: g, mu_cart, mu_pole
-g_u = 9.81 + 0.001
-g_l = 9.81 - 0.001
-mu_cart_u = 0.03 + 0.0001
-mu_cart_l = 0.03 - 0.0001
-mu_pole_u = 0.03 + 0.0001
-mu_pole_l = 0.03 - 0.0001
+g_u = 9.81 + 0.1
+g_l = 9.81 - 0.1
+mu_cart_u = 0.03 + 0.01
+mu_cart_l = 0.03 - 0.01
+mu_pole_u = 0.03 + 0.01
+mu_pole_l = 0.03 - 0.01
 N = 2
-# sampler = LatinHypercube(d=3, seed=0)
-# samples = scale(sampler.random(N), 
-#                 l_bounds=[g_l, mu_cart_l, mu_pole_l], 
-#                 u_bounds=[g_u, mu_cart_u, mu_pole_u])
-samples = np.array([[9.81, 0.03, 0.03], [9.81, 0.03, 0.03]])
+sampler = LatinHypercube(d=3, seed=0)
+samples = scale(sampler.random(N), 
+                l_bounds=[g_l, mu_cart_l, mu_pole_l], 
+                u_bounds=[g_u, mu_cart_u, mu_pole_u])
+# samples = np.array([[9.81, 0.03, 0.03], [9.81, 0.03, 0.03]])
 print(samples)
 
 # parameters
