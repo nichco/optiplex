d11b7c8ffcfbefb1a5b8e76de09cc43933c2b8c6
nichco <norndorff@live.com>
2026-07-14 11:47:19 -0700

    ff

--- examples/crm_aero_struct/distributed_crm_multipoint_bsplines_stress.py
+++ examples/crm_aero_struct/distributed_crm_multipoint_bsplines_stress.py
@@ -8,7 +8,7 @@ from subproblems_with_bsplines_and_stress import make_subproblem
 from optiplex import PlexC, Plex2, combo
 import tracemalloc
 
-num = 2 # number of operating conditions
+num = 4 # number of operating conditions
 sampler = LatinHypercube(d=2, seed=42)
 samples = scale(sampler.random(num), l_bounds=[0.4, 180], u_bounds=[0.6, 220])
 # samples = [(0.4135, 210), (0.4135, 207)] # test solution a
@@ -70,9 +70,9 @@ tracemalloc.start()
 opt = Plex2(subproblems=subPfuns,
             x_init=x_init,
             con=con,
-            mu=np.ones(n_twist_cp + n_thickness_cp) * 1, # N=2
+            # mu=np.ones(n_twist_cp + n_thickness_cp) * 1, # N=2
             # mu=np.ones((num - 1) * (n_twist_cp + n_thickness_cp)) * 1, # N=3
-            # mu=np.ones((135)) * 1, # N=4
+            mu=np.ones((135)) * 1, # N=4
             # mu=np.ones((378)) * 1, # N=6
             # mu=np.ones((729)) * 1, # N=8
             # mu=np.ones((1188)) * 1, # N=10
@@ -123,8 +123,8 @@ print('Total optimization time (s): ', opt_time[-1])
 # solution = np.load('examples/crm_aero_struct/test_solution_b.npz')
 # solution = np.load('examples/crm_aero_struct/test_solution_c.npz')
 # solution = np.load('examples/crm_aero_struct/solution_num_2_bsplines_and_stress.npz')
-solution = np.load('examples/crm_aero_struct/test_solution_d.npz')
-# solution = np.load('examples/crm_aero_struct/test_solution_N3.npz')
+# solution = np.load('examples/crm_aero_struct/test_solution_d.npz')
+solution = np.load('examples/crm_aero_struct/test_solution_N4.npz')
 alphas_star = solution['alphas']
 print('alphas_star (deg): ', np.rad2deg(alphas_star))
 twist_cp_star = solution['twist_cp']
--- examples/crm_aero_struct/timing_plot.py
+++ examples/crm_aero_struct/timing_plot.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+N = np.array([2, 4, 6, 8, 10])
+ALBCD_time = np.array([24.68, ])
 
-
-
-N_d = np.array([2, 4, 6])
-ALBCD_time = np.array([149.05, 309.84, 678.61])
+# N_d = np.array([2, 4, 6])
+# ALBCD_time = np.array([149.05, 309.84, 678.61])
 
 N_m = np.array([2, 4, 6, 8, 10])
 monolithic_time = np.array([9.27, 7.44, 13.27, 12.66])
